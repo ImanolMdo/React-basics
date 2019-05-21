8370f229e018bb9a5640071e8a98caee38e21369
Imanol PC <imanolmdev@gmail.com>
2019-05-21 19:19:32 +0200

    Managing pages with Reach Router

---
+++ src/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Router, Link } from "@reach/router";
+
+class Details extends React.Component {
+  render() {
+    return <header>
+              <Link to="/">Adopt Me!</Link>
+          </header>;;
+  }
+}
+
+export default Details;
